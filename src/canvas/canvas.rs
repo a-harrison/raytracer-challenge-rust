195a6431b3925cb71a9def072a4db39f2391161e
Dubois <dubois@Cerenas-MacBook-Pro-2.local>
2025-02-23 08:22:50 -0800

    Implemented pixel_at and write_pixel.

--- src/canvas/canvas.rs
+++ src/canvas/canvas.rs
@@ -2,14 +2,15 @@ use crate::entities::color::Color;
 
 trait CanvasContainer {
     fn create(width: usize, height: usize) -> Self; 
-    fn pixel_at(&self, x: usize, y: usize) -> Color; 
-    fn write_pixel(&self, x: usize, y: usize, c: Color);
+    fn pixel_at(&self, x: usize, y: usize) -> &Color; 
+    fn write_pixel(&mut self, x: usize, y: usize, c: Color);
 }
 
 struct VectorCanvas {
     width: usize, 
     height: usize, 
-    canvas: Vec<Vec<Color>>, 
+    canvas: Vec<Vec<Color>>,
+    default_color: Color 
 }
 
 impl CanvasContainer for VectorCanvas {
@@ -19,61 +20,22 @@ impl CanvasContainer for VectorCanvas {
         VectorCanvas {
             width: width, 
             height: height, 
-            canvas: vec![row.clone(); height]
+            canvas: vec![row.clone(); height],
+            default_color: Color::create(0_f64, 0_f64, 0_f64)
         }
     }
 
-    fn pixel_at(&self, x: usize, y: usize) -> Color {
-        let row = self.canvas.get(y);
-        let return_value = Color::create(0_f64,0_f64,0_f64);
-
-        self.canvas.get(y).get(x); 
-
-        match row {
-            Some(row) => {
-                let col = row.get(x); 
-                
-                match col {
-                    Some(col) => {
-                        return col.clone()
-                    },
-                    None => {}
-                }
-            },
-            None => {}
-        }
-        
-        return_value
+    // TODO: 
+    //  * Properly handle x, y outside of width, height. 
+    //  * Look into using Result<Color,Error?>
+    fn pixel_at(&self, x: usize, y: usize) -> &Color {
+        &self.canvas[x][y]
     }
 
-    fn write_pixel(&self, x: usize, y: usize, c: &Color) {
-        let row: Option<&Vec<Color>> = self.canvas.get(y); 
-
-        match row {
-            Some(row) = {
-
-            },
-            None => {
-                self.canvas.set
-            }
-        }
-
+    fn write_pixel(&mut self, x: usize, y: usize, c: Color) {
+        self.canvas[x][y] = c; 
     }
 }
-
-// fn get_row_from_canvas(canvas: &VectorCanvas, row: usize) -> Vec<Color> {
-//     let row = canvas.canvas.get(row); 
-
-//     match canvas.get(row) {
-//         Some(row) => {
-//             row
-//         }
-//     }    
-// }
-
-// fn get_color_from_row(canvas: &CanvasContainer, column: usize) -> Color {
-    
-// }
 
 #[cfg(test)]
 mod tests {
@@ -85,6 +47,22 @@ mod tests {
         
         assert_eq!(c.width, 10);
         assert_eq!(c.height, 20);
-        assert_eq!(c.canvas[0][0], Color::create(0.0, 0.0, 0.0)); 
+        
+        let row_iter = c.canvas.iter(); 
+        for row in row_iter {
+            for elem in row.iter() {
+                assert_eq!(elem.clone(), Color::create(0.0, 0.0, 0.));
+            }
+        }
+        // assert_eq!(c.canvas[0][0], Color::create(0.0, 0.0, 0.0)); 
+    }
+
+    #[test]
+    fn write_pixel_to_canvas() {
+        let mut c: VectorCanvas = VectorCanvas::create(10, 20);
+        let red: Color = Color::create(1.0, 0.0, 0.0);
+
+        c.write_pixel(2, 3, red);
+        assert_eq!(c.pixel_at(2, 3).clone(), Color::create(1.0, 0.0, 0.0));
     }
 }
